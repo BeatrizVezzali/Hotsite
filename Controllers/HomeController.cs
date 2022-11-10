f36d78e11a0b030e4b1d20bde3692d8d6b9cf51a
BeatrizVezzali <you@example.com>
2022-11-10 16:05:07 -0300

    Mais um projeto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Hotsite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Hotsite.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Cadastrar(Interesse cad)
+		{
+			try
+			{
+				DatabaseService dbs = new DatabaseService();
+				dbs.CadastraInteresse(cad);
+				return Json(new {status = "Ok", mensagem = "Sucesso!"});
+			}
+			catch (Exception e)
+			{
+				_logger.LogError("Erro no cadastro" + e.Message);
+
+				return Json(new {status = "ERR", mensagem = "Tente novamente!"});
+			}
+
+		}
+
+	}
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,44 @@
+﻿// @ts-nocheck
+$(document).ready(
+   function()
+   {
+	  $("#frmCadastro").submit(
+		function(e)
+		{
+           e.preventDefault();
+		   Cadastrar();
+		}
+	  )
+   }
+);
+
+function Cadastrar()
+{
+	let parametros = {
+
+		Nome: $("#nome").val(),
+		Email: $("#email").val(),
+		Mensagem: $("#mensagem").val()
+	};
+
+	$.post("Home/Cadastrar", parametros).done(
+		function(data)
+	{
+		if(data.status == "Ok")
+		{
+           $("#frmCadastro").after(data.mensagem)
+		   $("#frmCadastro").hide();
+		}
+		else
+		{
+			alert(data.mensagem);
+		}
+	})
+    .fail(function()
+	{
+      alert("Ocorreu um erro");
+	}
+	)
+};
+
+	
